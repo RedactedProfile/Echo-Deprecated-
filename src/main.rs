c49407dc4d68947743d887c542076c063db089b3
Kyle Harrison <redactedprofile@gmail.com>
2019-11-04 15:32:22 -0800

    TCP socket established - The humblest of beginnings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io;
+use std::io::Read;
+
+fn handle_client(stream: TcpStream) {
+    println!("hit")
+}
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:9875")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
 }
